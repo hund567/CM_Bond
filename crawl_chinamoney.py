6ccec33bd80b94fb510c0beca76354e7faf6e447
cong guan <hund567@163.com>
2020-07-18 12:30:12 +0800

    crawl

---
+++ crawl_chinamoney.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import time
+import json
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+#全局变量
+user = 'super'
+password = 'Password123'
+db_ip = "mysql57.rdsm05ltcjxv6y8.rds.bj.baidubce.com"
+db_name = 'guanc'
+
+
+def crawl():
+    print "time"
+
+if __name__ == '__main__':
+    crawl()
+
+
