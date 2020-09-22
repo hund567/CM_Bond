b0fdfcbb902b78b1c0682f6c9d78442a618a1fd5
管聪Srface <hund567@163.com>
2020-09-22 23:55:40 +0800

    get att from email server

--- modify_html.py
+++ modify_html.py
@@ -2,41 +2,43 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 
+
 def modify_html(html_file):
     content = open(html_file, 'r', encoding="utf-8")
     html_cont = content.read()
-    find_content = BeautifulSoup(html_cont, 'lxml')
-    src_list = ["./require.min.js", "./jquery.min.js"]
-    label_list = find_content.findAll('script')
-    i = 0
-    for label in label_list:
-        label["src"] = src_list[i]  # src_list[i]类型也是<class 'bs4.element.Tag'>
-        i += 1
-        if i == 2:
-            break
+    find_content = BeautifulSoup(html_cont.replace('&nbsp;', ' '), 'lxml')
 
-    #删除掉最后一个pre对象
     label_list_pre = find_content.findAll('pre')
-
-    remove_list = ["[NbConvertApp] WARNING","This application is used to convert"]
+    remove_list = ["[NbConvertApp] WARNING", "This application is used to convert"]
 
     for each_remove_line in remove_list:
         i = 0
         for each in label_list_pre:
-            if each_remove_line  in str(each):
+            if each_remove_line in str(each):
                 tag_num = i
                 break
             i = i + 1
 
         new_tag = find_content.new_tag("pre")
-        new_tag.string = "detail"
-        # `replace_with` 方法来替换掉
+        new_tag.string = ''
         find_content.find_all('pre')[tag_num].replace_with(new_tag)
 
-    # 最后将其写入
-    with open(html_file, 'w') as fp:
+    with open(html_file, 'w', encoding="utf-8") as fp:
+        fp.write(find_content.prettify())
+
+    src_list = ["./require.min.js", "./jquery.min.js"]
+    label_list = find_content.findAll('script')
+    i = 0
+    for label in label_list:
+        label["src"] = src_list[i]
+        i += 1
+        if i == 2:
+            break
+    with open(file_path + "BondDaily-v4-without-js.html", 'w', encoding="utf-8") as fp:
         fp.write(find_content.prettify())
 
 
-if __name__ =="__main__":
-    modify_html("C:\\Users\\Administrator\\Desktop\\html\\BondDaily-v4.html")
+if __name__ == "__main__":
+    file_path = "C:\\Users\\Administrator\\Desktop\\html\\"
+    file_name = "BondDaily-v4.html"
+    modify_html(file_path + file_name)
--- sendmail.py
+++ sendmail.py
@@ -7,11 +7,7 @@ from email.header import Header
 import datetime
 
 
-def sendmail(file_path,sender,receivers):
-    # 第三方 SMTP 服务
-    # mail_host = "smtp.163.com"  # 设置服务器
-    # mail_user = "hund567"  # 用户名
-    # mail_pass = "1990312peking"  # 口令
+def sendmail(file_path, sender, receivers):
     #
     mail_host = "smtp.sina.com"  # 设置服务器
     mail_user = "potatooo43@sina.com"  # 用户名
@@ -21,7 +17,7 @@ def sendmail(file_path,sender,receivers):
     message = MIMEMultipart()
     today = datetime.datetime.today().date()
     message['From'] = mail_user
-    subject = str(today)+"债券市场综合报表"
+    subject = str(today) + "债券市场综合报表"
     message['Subject'] = Header(subject, 'utf-8')
 
     # 邮件正文内容
@@ -29,19 +25,22 @@ def sendmail(file_path,sender,receivers):
 
     # 构造附件1，传送当前目录下的 test.txt 文件
 
-    att1 = MIMEText(open(file_path, 'rb').read(), 'base64', 'utf-8')
+    att1 = MIMEText(open(file_path + "BondDaily-v4.html", 'rb').read(), 'base64', 'utf-8')
     att1["Content-Type"] = 'application/octet-stream'
     # 这里的filename可以任意写，写什么名字，邮件中显示什么名字
-    filename = str(today)+"_BondDaily.html"
+    filename = str(today) + "_BondDaily_js.html"
     print(filename)
-    att1["Content-Disposition"] = 'attachment; filename='+filename
+    att1["Content-Disposition"] = 'attachment; filename=' + filename
     message.attach(att1)
 
     # # 构造附件2，传送当前目录下的 runoob.txt 文件
-    # att2 = MIMEText(open('runoob.txt', 'rb').read(), 'base64', 'utf-8')
-    # att2["Content-Type"] = 'application/octet-stream'
-    # att2["Content-Disposition"] = 'attachment; filename="runoob.txt"'
-    # message.attach(att2)
+    att2 = MIMEText(open(file_path + "BondDaily-v4-without-js.html", 'rb').read(), 'base64', 'utf-8')
+    att2["Content-Type"] = 'application/octet-stream'
+    # 这里的filename可以任意写，写什么名字，邮件中显示什么名字
+    filename = str(today) + "_BondDaily_without_js.html"
+    print(filename)
+    att2["Content-Disposition"] = 'attachment; filename=' + filename
+    message.attach(att2)
 
     try:
         smtpObj = smtplib.SMTP()
@@ -57,6 +56,7 @@ def sendmail(file_path,sender,receivers):
 
 if __name__ == '__main__':
     sender = 'potatooo43@sina.com'
-    # sender = 'hund567@163.com'
-    receivers = ['hund567@163.com',"227362424@qq.com","wingyylr@163.com","linyingyingalm@cmbchina.com"]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱 并且注意receivers是个list里面可以放多个目标邮箱地址
-    sendmail("/Users/hund567/Desktop/code_pycharm/CMB_BOND/BondDaily-v2.html",sender,receivers)
+    receivers = ['hund567@163.com', "227362424@qq.com", "wingyylr@163.com",
+                 "linyingyingalm@cmbchina.com"]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱 并且注意receivers是个list里面可以放多个目标邮箱地址
+    file_path = "C:\\Users\\Administrator\\Desktop\\html\\"
+    sendmail(file_path, sender, receivers)
